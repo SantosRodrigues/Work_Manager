5d00d24a78905e36d575cf91b630a17c3dc55289
Hsantos <higor.rx@gmail.com>
2025-06-26 22:51:27 -0300

    add constraints

--- app/src/main/java/com/example/workmanagertest/WorkSetup.kt
+++ app/src/main/java/com/example/workmanagertest/WorkSetup.kt
@@ -1,7 +1,9 @@
 package com.example.workmanagertest
 
 import androidx.work.BackoffPolicy
+import androidx.work.Constraints
 import androidx.work.Data
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.PeriodicWorkRequestBuilder
 import java.util.concurrent.TimeUnit
@@ -16,11 +18,18 @@ class WorkSetup {
             .setBackoffCriteria(BackoffPolicy.LINEAR, 1L, TimeUnit.MINUTES)          //1,2,3,4,5
 //                .setBackoffCriteria(BackoffPolicy.EXPONENTIAL, Duration.ofMinutes(1))         //1,2,4,8,16
             .addTag("ping")
+            .setConstraints(
+                Constraints(
+                    requiredNetworkType = NetworkType.UNMETERED,
+                    requiresCharging = true,
+                    requiresBatteryNotLow = true,
+                    requiresStorageNotLow = true
+                )
+            )
             .setInputData(data)
             .build()
 
-        val uuid =
-            work.id                                                                       //UUID do work
+        val uuid = work.id                                                           //UUID do work
     }
 
     fun setupPeriodicWork(data: Data) =
